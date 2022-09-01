9f8b95ff9cbcfccfb8490b088b8fca3be57f699c
Itzhak Bokris <jzak300@gmail.com>
2022-09-01 12:19:27 +0300

    Create script that setup and run wasm-dymension local node

--- celestia-light-node/README.md
+++
@@ -1,7 +0,0 @@
-# Celestia Light Node
-Build and run celestia light client
-```sh
-# inside the project folder:
-docker build -t celestia-light-node celestia-light-node/.
-docker run -d --rm -p 26658:26658 -p 9090:9090 --name celestia-light-node celestia-light-node
-```
--- scripts/setup_and_run_node.sh
+++ scripts/setup_and_run_node.sh
@@ -2,10 +2,10 @@
 
 CHAIN_ID=${CHAIN_ID:-dymn-devnet}
 MONIKER=${MONIKER:-dymn-devnet-validator}
-KEY_NAME=${MONIKER:-dymn-devnet-key}
+KEY_NAME=${KEY_NAME:-dymn-devnet-key}
+NAMESPACE_ID=${NAMESPACE_ID:-000000000000FFFF}
 TOKEN_AMOUNT=${TOKEN_AMOUNT:-10000000000000000000000000uwasm}
 STAKING_AMOUNT=${STAKING_AMOUNT:-1000000000uwasm}
-DA_BASE_URL=${DA_BASE_URL:-'http://0.0.0.0:26658'}
 
 rm -rf "$HOME"/.wasmd
 
@@ -17,15 +17,18 @@ sed -i'' -e '/\[api\]/,+3 s/enable *= .*/enable = true/' "$HOME"/.wasmd/config/a
 sed -i'' -e "s/^chain-id *= .*/chain-id = \"$CHAIN_ID\"/" "$HOME"/.wasmd/config/client.toml
 sed -i'' -e '/\[rpc\]/,+3 s/laddr *= .*/laddr = "tcp:\/\/0.0.0.0:26657"/' "$HOME"/.wasmd/config/config.toml
 sed -i'' -e 's/"time_iota_ms": "1000"/"time_iota_ms": "10"/' "$HOME"/.wasmd/config/genesis.json
+sed -i'' -e 's/bond_denom": ".*"/bond_denom": "uwasm"/' "$HOME"/.wasmd/config/genesis.json
+sed -i'' -e 's/mint_denom": ".*"/mint_denom": "uwasm"/' "$HOME"/.wasmd/config/genesis.json
 
-wasmd keys add "$KEY_NAME" --keyring-backend test
-wasmd add-genesis-account "$KEY_NAME" "$TOKEN_AMOUNT" --keyring-backend test
-wasmd gentx "$KEY_NAME" "$STAKING_AMOUNT" --chain-id "$CHAIN_ID" --keyring-backend test
+wasmd keys add "$KEY_NAME"
+wasmd add-genesis-account "$KEY_NAME" "$TOKEN_AMOUNT"
+wasmd gentx "$KEY_NAME" "$STAKING_AMOUNT" --chain-id "$CHAIN_ID"
+wasmd collect-gentxs
 
-wasmd start --optimint.aggregator true \
-  --optimint.da_layer celestia \
-  --optimint.da_config "{\"base_url\":\"$DA_BASE_URL\",\"timeout\":60000000000,\"gas_limit\":6000000,\"namespace_id\":[0,0,0,0,0,0,255,255]}" \
-  --optimint.namespace_id 000000000000FFFF \
-  --optimint.da_start_height 21380 \
-  --optimint.block_time 30s \
-  --optimint.da_block_time 30s
+wasmd start --dymint.aggregator true \
+  --dymint.da_layer mock \
+  --dymint.settlement_config "{\"root_dir\": \"$HOME/.wasmd\", \"db_path\": \"data\"}" \
+  --dymint.settlement_layer mock \
+  --dymint.block_batch_size 1000 \
+  --dymint.namespace_id "$NAMESPACE_ID" \
+  --dymint.block_time 1s
